ca10f6cdfd1d9cb25d5942becc16e18e5367061b
Pablo Martinez-Abrego Gonzalez <pmarti22@student00.cse.nd.edu>
2020-08-22 19:06:23 -0400

    challenge02: done

--- challenge02/program.cpp
+++ challenge02/program.cpp
@@ -7,9 +7,11 @@
 #include <stack>
 using namespace std;
 
+// Function that checks wether HTML tags are balanced
 bool check_balance(string s) {
 	stack<char> left;
 
+	// Push complete opening TAG to the stack
 	for (int i = 0; i < s.size(); i++) {
 		if(s[i] == '<' && s[i+1] != '/') {
 			for(int j = i+1; j < s.size(); j++) {
@@ -18,17 +20,21 @@ bool check_balance(string s) {
 				}
 				left.push(s[j]);
 			}
-		} else if(s[i] == '/') {
-			while(s[i] != '>'){
+		// Go to the last character of closing TAG to iterate backwards
+		} else if(s[i] == '/' && s[i-1] == '<') {
+			while(s[i] != '>' && i <= s.size()){
 				i++;
 			}
-			int temp = i--;
+			int temp = i--; // Temporary variable for i after iterating backwards
+			// Iterate backwards to compare complete TAG to the TAG in stack
 			while(true){
+				if(i+1 > s.size() || i < 0 || left.empty()) {
+					return false;
+				}
 				if (s[i] == left.top()){
 					left.pop();
 					i--;
-				}
-				else {
+				} else {
 					return false;
 				}
 				if(s[i] == '/') {
@@ -41,6 +47,7 @@ bool check_balance(string s) {
 	return left.empty();
 }
 
+// Main function
 int main() {
 	string line;
 
