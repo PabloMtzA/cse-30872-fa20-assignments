d4f6939a63dbe8938426d687a39ee23d599f28ad
Pablo Martinez-Abrego Gonzalez <pmarti22@student00.cse.nd.edu>
2020-09-12 23:10:10 -0400

    challenge07: done

--- challenge07/program.cpp
+++ challenge07/program.cpp
@@ -13,36 +13,36 @@ bool check_numbers(int n, int d) {
   int dsize = to_string(d).length();
 
   // Add zero at end for comparison purposes
-  if(nsize == 4) {
+  if (nsize == 4) {
     n = n * 10;
   }
-  if(dsize == 4) {
+  if (dsize == 4) {
     d = d * 10;
   }
 
-  if(nsize < 4 || dsize < 4) {
+  if (nsize < 4 || dsize < 4) {
     return false;
   }
 
   string nstring = to_string(n);
-  for(int i = 0; i < nstring.size(); i++){
-    for(int j = i+1; j < nstring.size(); j++) {
-      if(nstring[i] == nstring[j]) {
+  for (int i = 0; i < nstring.size(); i++) {
+    for (int j = i+1; j < nstring.size(); j++) {
+      if (nstring[i] == nstring[j]) {
         return false;
       }
     }
   }
   string dstring = to_string(d);
-  for(int i = 0; i < dstring.size(); i++){
-    for(int j = i+1; j < dstring.size(); j++) {
-      if(dstring[i] == dstring[j]) {
+  for (int i = 0; i < dstring.size(); i++) {
+    for (int j = i+1; j < dstring.size(); j++) {
+      if (dstring[i] == dstring[j]) {
         return false;
       }
     }
   }
 
-  for(int i = 0; i < nstring.size(); i++) {
-    for(int j = 0; j < dstring.size(); j++) {
+  for (int i = 0; i < nstring.size(); i++) {
+    for (int j = 0; j < dstring.size(); j++) {
       if (nstring[i] == dstring[j]) {
         return false;
       }
@@ -50,11 +50,9 @@ bool check_numbers(int n, int d) {
   }
 
   return true;
-
 }
 
 void perms(int n) {
-
   bool found = false;
   const int MIN_DENOM = 01234;
   const int MAX_NUM = 98765;
@@ -65,12 +63,12 @@ void perms(int n) {
 
   while (numer <= MAX_NUM) {
     numer = denom * n;
-      if(check_numbers(numer, denom)) {
+      if check_numbers(numer, denom)) {
         found = true;
         int nsize = to_string(numer).length();
         int dsize = to_string(denom).length();
 
-        if(nsize == 4) {
+        if (nsize == 4) {
           cout << "0" << numer << " / " << denom++ << " = " << n << endl;
         } else if (dsize == 4) {
           cout << numer << " / " << "0" << denom++ << " = " << n << endl;
@@ -81,10 +79,9 @@ void perms(int n) {
     denom++;
   }
 
-  if(!found) {
+  if (!found) {
     cout << "There are no solutions for " << n << "." << endl;
   }
-
 }
 
 
